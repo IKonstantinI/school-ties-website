280963a1e5162fde8910ab65635edf7b738bc321
IKonstantinI <149531288+IKonstantinI@users.noreply.github.com>
2024-11-10 23:16:35 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const carousel = document.querySelector('.product-carousel');
-    // Реализовать логику карусели
-});
+// Мобильное меню
+const menuToggle = document.querySelector('.menu-toggle');
+const navUl = document.querySelector('nav ul');
+const menuItems = document.querySelectorAll('nav ul li a'); // Выбираем все ссылки в меню
+
+if (menuToggle && navUl) {
+    // Переключение видимости меню при клике на кнопку
+    menuToggle.addEventListener('click', () => {
+        navUl.classList.toggle('showing');
+    });
+}
+
+if (menuItems.length > 0) {
+    // Добавляем обработчик клика на каждый пункт меню
+    menuItems.forEach(item => {
+        item.addEventListener('click', () => {
+            navUl.classList.remove('showing'); // Скрываем меню
+        });
+    });
+}
